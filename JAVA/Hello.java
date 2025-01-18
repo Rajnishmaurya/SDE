7ee94e2223d95a83386e71b5d21941f9e993edc8
Rajnishmaurya <mauryarajnish73@gmail.com>
2025-01-18 22:48:32 +0530

    software engineering

--- JAVA/Hello.java
+++ JAVA/Hello.java
@@ -1,5 +1,5 @@
 class Hello {
-    public static void main(String a[])
+    public static void main(String[] args)
     {
         System.out.println("Hello World");
         System.out.println(3+5);
@@ -8,37 +8,37 @@ class Hello {
         int num=3;
         System.out.println(num);
 
-       int num1=0;
-       byte by=127;
-       short sh=558;
-       long l=5854l;
+        // int num1=0;
+        // byte by=127;
+        // short sh=558;
+        // long l=5854l;
 
-       float f=5.8f;
-       double d=5.8;
-       char c='k';
-       boolean b=true;
-
-
-       int num2=0b10101;
-       System.out.println(num2);
-
-       double num4=12e10;
-       System.out.println(num4);
-
-       char ch='a';
-       System.out.println(ch);
+        // float f=5.8f;
+        // double d=5.8;
+        // char c='k';
+        // boolean b=true;
 
 
-       //type casting
-       byte bt=127;
+        // int num2=0b10101;
+        // System.out.println(num2);
 
-       int at=bt;
-       System.out.println(bt);
-       System.out.println(at);
+        // double num4=12e10;
+        // System.out.println(num4);
 
-       int k=12;
-       byte bk=(byte)k; // type casting
-       System.out.println(bk);
+        // char ch='a';
+        // System.out.println(ch);
+
+
+        // //type casting
+        // byte bt=127;
+
+        // int at=bt;
+        // System.out.println(bt);
+        // System.out.println(at);
+
+        // int k=12;
+        // byte bk=(byte)k; // type casting
+        // System.out.println(bk);
     }    
 }
 
